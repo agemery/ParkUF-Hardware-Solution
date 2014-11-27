00387aa58051cf66e8e9d2feedf24bd5ca81e2e4
aemery <agemery2@gmail.com>
2014-11-27 00:09:44 -0500

    should be working in full now. So you can can run the sketch (it is currently set up for 1 sensor), detect cars and post to the server. Currently it waits until the serial monitor is open to start running, so uncomment that line in the setup method to change this. kthxbai

--- master/master.ino
+++ master/master.ino
@@ -1,50 +1,25 @@
 #include <Process.h>
-
-  //lets get some things straight
-  //this never changes
-  const String webAddress = "http://54.148.158.124/parking/"; 
-  //this changes depending on the lot #
-  const String lot = "35/";
-  //this changes based on the direction (entry or exit)
-  const String direct = "entry";
-
-  //Sonar stuff
-  const int sonarPin = 8; //the io pin we are using
-  const float threshhold = 265; //distance threshold in centimeters 
-  //anything under threshold counts as a car
-
-  //PIR stuff
-  const int calibrationTime = 10; //seconds to allow calibration
-  const int pirPin = 11;
+  
+  //Forgive me father for I have used Global Variables
 
 void setup() {
   Bridge.begin();
   Serial.begin(9600);
+  
+  //we will probably take this out when using in the field
   while(!Serial); //wait for serial monitor duh
   Serial.println("Serial up now");
   
-  
+  //setting up sensors
+  setupSensors();
 }
 
 void loop() {
-  delay(5000);
-  Serial.println("Running curl.");
-  runCurl();
-
-}
-
-void runCurl() {
-  Process p;
-  p.begin("curl");
-  p.addParameter("-X");
-  p.addParameter("PUT");
-  p.addParameter(webAddress + lot + direct);
-  p.run();
-  
-  while(p.available()>0) {
-    char c = p.read();
-    Serial.print(c);
+  Serial.println("Polling for cars.");
+  if (isCarDetected()) { //if a car is detected update the server
+    updateServer();
   }
-  Serial.flush();
-  Serial.print("\n");
+
 }
+
+
